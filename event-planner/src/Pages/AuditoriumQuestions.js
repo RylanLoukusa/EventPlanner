111f1b699326e00717262a953ff473575c8ba174
Fire-fly630 <75851942+Fire-fly630@users.noreply.github.com>
2024-03-21 17:09:53 -0500

    Update AuditoriumQuestions.js

--- event-planner/src/Pages/AuditoriumQuestions.js
+++ event-planner/src/Pages/AuditoriumQuestions.js
@@ -5,8 +5,8 @@ import {
   TouchableOpacity,
   ScrollView,
   View,
+  StyleSheet
 } from 'react-native';
-import { CheckBox } from 'react-native-elements';
 
 
 
@@ -42,3 +42,56 @@ export default function AuditoriumQuestions(){
   </View>
   );
 }
+const Styles = StyleSheet.create({
+
+Setup: {
+  backgroundColor: 'white',
+  alignItems: 'center',
+},
+
+Buttons: {
+  backgroundColor: 'orange',
+  margin: 10,
+},
+
+ButtonText: {
+  textAlign: 'center',
+},
+
+SelectionView: {
+  borderWidth: 4,
+  padding: 5,
+  paddingBottom: 10,
+  margin: 10,
+},
+
+HeaderText: {
+  fontWeight: 'bold',
+  textSize: 25,
+},
+
+TitleText: {
+  fontSize: 40,
+  paddingBottom: 50,
+  color: 'grey',
+  textAlign: 'center',
+},
+
+Input:{
+  padding:5,
+  margin:20, 
+  borderWidth:1,
+},
+
+Header:{
+  padding: 20, 
+  textAlign: 'center', 
+  fontSize: 20,
+},
+
+InputPrompt:{
+  color: 'orange',
+  fontSize: 15,
+  margin: 30,
+}
+});
