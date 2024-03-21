98b50d4c86270b18fd44ad5450b35360c0f5628d
Fire-fly630 <75851942+Fire-fly630@users.noreply.github.com>
2024-03-21 17:11:18 -0500

    Update FieldTripQuestions.js

--- event-planner/src/Pages/FieldTripQuestions.js
+++ event-planner/src/Pages/FieldTripQuestions.js
@@ -4,6 +4,7 @@ import {
   TextInput,
   TouchableOpacity,
   View,
+  StyleSheet
 } from 'react-native';
 
 export default function FieldTripQuestions(){
@@ -23,3 +24,56 @@ export default function FieldTripQuestions(){
 </View>
  );
 }
+const Styles = StyleSheet.create({
+
+Setup: {
+  backgroundColor: 'white',
+  alignItems: 'center',
+},
+
+Buttons: {
+  backgroundColor: 'orange',
+  margin: 10,
+},
+
+ButtonText: {
+  textAlign: 'center',
+},
+
+SelectionView: {
+  borderWidth: 4,
+  padding: 5,
+  paddingBottom: 10,
+  margin: 10,
+},
+
+HeaderText: {
+  fontWeight: 'bold',
+  textSize: 25,
+},
+
+TitleText: {
+  fontSize: 40,
+  paddingBottom: 50,
+  color: 'grey',
+  textAlign: 'center',
+},
+
+Input:{
+  padding:5,
+  margin:20, 
+  borderWidth:1,
+},
+
+Header:{
+  padding: 20, 
+  textAlign: 'center', 
+  fontSize: 20,
+},
+
+InputPrompt:{
+  color: 'orange',
+  fontSize: 15,
+  margin: 30,
+}
+});
