ae55dd75fcb4d47ca94b1b2c033cad5d8fcc90e3
Fire-fly630 <75851942+Fire-fly630@users.noreply.github.com>
2024-03-21 17:09:32 -0500

    Update AdminView.js

--- event-planner/src/Pages/AdminView.js
+++ event-planner/src/Pages/AdminView.js
@@ -1,38 +1,50 @@
 import React from 'react';
 import {
-  Image,
-  ScrollView,
   Text,
   TouchableOpacity,
   View,
+  StyleSheet
 } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
+
+
 function UpcomingEvents() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style = {{padding: 30, backgroundColor: 'white'}}>
+    <TouchableOpacity><Text style={{ textAlign: 'right', margin: 20, fontSize: 20}}>Add A Host</Text></TouchableOpacity>
+    <View style = {Styles.Setup}>
+    <Text> Date </Text>
+    <Text> Party name and type </Text>
+    </View>
     </View>
   );
 }
 
 function PastEvents() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={Styles.Setup}>
     </View>
   );
 }
 
-function CreateEvents() {
+function CreateEvents({navigation}) {
   return (
-    <View style={{ flex: 1}}>
-      <Text style={{padding: 15, margin: 20, textAlign: 'center', fontSize: 45, color: 'grey'}}>What kind of booking do you require?</Text>
-      <View style={{alignItems: 'center', borderWidth: 4, margin: 20}}>
-        <Text style={{fontSize: 25}}>Auditorium Rental</Text>
-        <TouchableOpacity style={{backgroundColor: 'orange', margin:15, padding: 5}}><Text>Book Now</Text></TouchableOpacity>
-        <Text style={{fontSize: 25}}>Third Floor Event</Text>
-        <TouchableOpacity style={{backgroundColor: 'orange', margin:15, padding: 5}}><Text>Book Now</Text></TouchableOpacity>
-        <Text style={{fontSize: 25}}>Field Trip</Text>
-        <TouchableOpacity style={{backgroundColor: 'orange', margin:15, padding: 5}}><Text>Book Now</Text></TouchableOpacity>
+    <View style={Styles.Setup}>
+      <Text style={Styles.TitleText}>What kind of booking do you require?</Text>
+      <View style={Styles.SelectionView}>
+        
+        
+        <Text style={Styles.Header}>Auditorium Rental</Text>
+        <TouchableOpacity  onPress={() => navigation.navigate('AuditoriumQuestions')} style={Styles.Buttons}><Text style = {Styles.ButtonText}>Book Now</Text></TouchableOpacity>
+
+
+        <Text style={Styles.Header}>Third Floor Event</Text>
+        <TouchableOpacity  onPress={() => navigation.navigate('ThirdFloorQuestions')} style={Styles.Buttons}><Text style = {Styles.ButtonText}>Book Now</Text></TouchableOpacity>
+
+
+        <Text style={Styles.Header}>Field Trip</Text>
+        <TouchableOpacity  onPress={() => navigation.navigate('FieldTripQuestions')} style={Styles.Buttons}><Text style = {Styles.ButtonText}>Book Now</Text></TouchableOpacity>
       </View>
     </View>
   );
@@ -41,7 +53,7 @@ function CreateEvents() {
 const Drawer = createDrawerNavigator();
 
 
-export default function AdminViewScreen(){
+export default function AdminViewScreen({navigation}){
    return (
     <Drawer.Navigator>
       <Drawer.Screen name="Upcoming Events" component={UpcomingEvents} />
@@ -50,3 +62,56 @@ export default function AdminViewScreen(){
     </Drawer.Navigator>
   );
 }
+const Styles = StyleSheet.create({
+
+Setup: {
+  backgroundColor: 'white',
+  alignItems: 'center',
+},
+
+Buttons: {
+  backgroundColor: 'orange',
+  margin: 10,
+},
+
+ButtonText: {
+  textAlign: 'center',
+},
+
+SelectionView: {
+  borderWidth: 4,
+  padding: 5,
+  paddingBottom: 10,
+  margin: 10,
+},
+
+HeaderText: {
+  fontWeight: 'bold',
+  textSize: 25,
+},
+
+TitleText: {
+  fontSize: 40,
+  paddingBottom: 50,
+  color: 'grey',
+  textAlign: 'center',
+},
+
+Input:{
+  padding:5,
+  margin:20, 
+  borderWidth:1,
+},
+
+Header:{
+  padding: 20, 
+  textAlign: 'center', 
+  fontSize: 20,
+},
+
+InputPrompt:{
+  color: 'orange',
+  fontSize: 15,
+  margin: 30,
+}
+});
